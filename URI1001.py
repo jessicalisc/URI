6560b0f595b2bc6c216787df4a447002d896f200
jessicalisc <jessicalisc@hotmail.com>
2019-05-07 20:08:28 -0300

    Primeiros exercicios

---
+++ URI1001.py
@@ -0,0 +1,11 @@
+#Exercicio 1001 URI
+
+A = int(input()) # Recebe entrada atraves de input converte para int e armazena em A
+B = input() # Recebe a entrada atraves de unput
+B = int(B) #Converte B para int e armazena em B
+
+X = A + B #Faz a soma entre A e B, armazena o resultado em X
+
+print("X =",X) # Exibe X da forma proposta
+
+
---
+++ URI1003.py
@@ -0,0 +1,8 @@
+#Exercicio URI 1003
+
+A = int(input()) #Recebe a entrada em formato String converte para int e armazena em A
+B = int(input()) #Recebe a entrada em formato String converte para int e armazena em B
+
+SOMA = A + B #Operação de soma entre A e B armazenada em SOMA
+
+print("SOMA =",SOMA)
