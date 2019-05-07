05769a280ce82e037d708e29e72b9887daa968b7
jessicalisc <jessicalisc@hotmail.com>
2019-05-07 20:26:02 -0300

    Exercicios URI

---
+++ URI1004.py
@@ -0,0 +1,8 @@
+#Exercicio URI 1004
+
+A = int(input()) #Recebendo o valor da variavel A e atribuindo o valor a variavel
+B = int(input()) #Recendo o valor da variavel B e  convertendo o valor da variavel B
+
+PROD = A * B  #Fazendo a operação de multiplicação entre as variaveis A e B, gerando o produto
+
+print("PROD =",PROD)
